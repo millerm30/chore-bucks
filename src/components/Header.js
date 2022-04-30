a3eb4984a966e9a561f0cfe4aedae39030b9ca90
millerm30 <millergm30@gmail.com>
2022-04-29 22:39:34 -0230

    hero section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import AppLogo from '../assets/appLogo.png'
-import { RiShoppingBasketLine } from "react-icons/ri";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
 const Header = () => {
   return (
@@ -8,9 +8,10 @@ const Header = () => {
       <div className='appLogo flex content-center'>
         <img src={AppLogo} alt='' className='pl-2'/>
         <h1 className='self-center'><span className='text-3xl text-blue-800 font-bold'>Chore</span><span className='text-3xl text-green-800 font-bold'>Bucks</span></h1>
+        <div className='mt-2 w-2 h-6 border-2 border-b-gray-900 border-t-red-700 border-l-yellow-600 border-r-green-600 rounded-full animate-spin'></div>
       </div>
       <div className='shoppingBasket flex items-end'>
-        <RiShoppingBasketLine className='text-5xl text-yellow-600' />
+        <RiMoneyDollarCircleLine className='text-5xl text-yellow-600' />
         <span className='pointsValue pr-2'>
           <p className='text-center font-semibold'>0</p>
           <p>Points</p>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DiBitbucket } from 'react-icons/di'
 
 const Hero = () => {
   return (
@@ -8,6 +9,24 @@ const Hero = () => {
         <h4 className='text-1xl font-bold p-1'>Complete your chore to add points to your bank!</h4>
         <p className='p-1'>Each point earned will value .25&#162; Maximum of 10 points per chore.</p>
         <p className='p-1'>Drop completed chores into your bucket</p>
+      </div>
+      <div className='myForm pt-5'>
+        <form className='flex flex-col w-1/3 mx-auto'>
+          <label for='chores' className='mb-3'>Choose your chore: </label>
+          <select name='chores' className='rounded-md py-2 border border-blue-700 rounded outline-none'>
+            <option value='/' default>Choose Your Chore</option>
+            <option value='Feed Pet'>Feed Family Pet</option>
+            <option value='Make Bed'>Make Bed</option>
+            <option value='Fold Laundry'>Fold Laundry</option>
+            <option value='Wash Dishes'>Wash Dishes</option>
+            <option value='Empty Dishwasher'>Empty Dishwasher</option>
+            <option value='Clean Room'>Clean Bedroom</option>
+          </select>
+        </form>
+          <button className='btnSubmit mt-5 bg-blue-400 hover:bg-blue-500 text-black font-bold py-2 px-4 border border-blue-700 rounded '>Add Chore</button>
+        <div className='bucket flex justify-end'>
+            <DiBitbucket className='text-8xl text-orange-600'/>
+        </div>
       </div>
     </div>
   );
