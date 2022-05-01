a759c9c2447d3d541aa9fb1a469409bb6e8f1ea9
millerm30 <millergm30@gmail.com>
2022-05-01 14:46:04 -0230

    hero changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="heroContainer text-center bg-hero-pattern bg-no-repeat bg-center bg-cover h-96 flex items-center justify-center">
-      <div className="appInfo">
-        <h2 className="text-2xl font-semibold p-1">
+    <div className="heroContainer text-center bg-hero-pattern bg-no-repeat bg-center bg-cover h-96 flex flex-col items-center justify-start">
+      <div className="mainInfo">
+        <h2 className="text-3xl p-1 mt-5 mb-5 font-bold">
           Kids earn points for completing chores!
         </h2>
+      </div>
+      <div className='secondInfo'>
         <h4 className="text-1xl font-bold p-1">
           Complete your chore to add points to your bank!
         </h4>
