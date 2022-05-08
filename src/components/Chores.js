e8046a73eae259073d94cc385d86f26a68e8fe8a
millerm30 <millergm30@gmail.com>
2022-05-08 12:47:13 -0230

    mobile sizing completion

--- src/components/Chores.js
+++ src/components/Chores.js
@@ -15,7 +15,7 @@ const Chores = () => {
            <p className="italic pt-4">No Chores to do!</p>
          )}
          {
-           <div className="grid grid-cols-2 gap-5 mx-5 md:grid-cols-4">
+           <div className="grid grid-cols-2 gap-5 mx-5 md:grid-cols-3 lg:grid-cols-4">
              {chores.map((chore) => (
                <div className="bg-[#f8f8f8] w-full px-2 py-1 flex flex-col justify-start items-center border-2 border-blue-400 rounded-lg">
                  <button
@@ -23,7 +23,7 @@ const Chores = () => {
                    className="text-2xl text-red-600 self-end">
                    <GoTrashcan />
                  </button>
-                 <h2 className="choreName text-xl py-3">{chore}</h2>
+                 <h2 className="choreName text-md md:text-xl py-3">{chore}</h2>
                  <button
                  onClick={() => completeChore(chore)}
                  className="bg-blue-400 mb-4 self-center px-4 py-2 border-2 border-blue-600 rounded-lg transition ease-in-out hover:bg-blue-500">
