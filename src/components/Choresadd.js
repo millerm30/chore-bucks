3f8acf3fca4d72c9b6edcd11a50bb026a2b8ccaf
millerm30 <millergm30@gmail.com>
2022-05-08 14:59:48 -0230

    remove chores list from component

--- src/components/Choresadd.js
+++ src/components/Choresadd.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useChores } from '../contexts/Chores'
+import choresChoices from './Chorelist'
 
 const Hero = () => {
   const { addChore } = useChores();
@@ -14,17 +15,6 @@ const Hero = () => {
     e.preventDefault();
     addChore(chore);
   };
-
-  const choresChoices = [
-    { label: '', value: '' },
-    { label: 'Feed Family Pet', value: 'Feed Family Pet', id: 1 },
-    { label: 'Make Bed', value: 'Make Bed', id: 2 },
-    { label: 'Clean Bedroom', value: 'Clean Bedroom', id: 3 },
-    { label: 'Wash Dishes', value: 'Wash Dishes', id: 4 },
-    { label: 'Empty Dishwasher', value: 'Empty Diswasher', id: 5 },
-    { label: 'Fold Laundry', value: 'Fold Laundry', id: 6 },
-    { label: 'Pickup Toys', value: 'Pickup Toys', id :7 },
-  ];
   
   return (
     <div className="choreaddContainer text-center">
