37d544d698ae5e11d1fd5455619b5f1232439e5f
millerm30 <millergm30@gmail.com>
2022-05-08 15:27:44 -0230

    add chores to choreList

--- src/components/Chorelist.js
+++ src/components/Chorelist.js
@@ -1,12 +1,89 @@
 const choresChoices = [
-  { label: "", value: "" },
-  { label: "Feed Family Pet", value: "Feed Family Pet", id: 1 },
-  { label: "Make Bed", value: "Make Bed", id: 2 },
-  { label: "Clean Bedroom", value: "Clean Bedroom", id: 3 },
-  { label: "Wash Dishes", value: "Wash Dishes", id: 4 },
-  { label: "Empty Dishwasher", value: "Empty Diswasher", id: 5 },
-  { label: "Fold Laundry", value: "Fold Laundry", id: 6 },
-  { label: "Pickup Toys", value: "Pickup Toys", id: 7 },
+  { 
+    label: "", 
+    value: "" 
+  },
+  { 
+    label: "Feed Family Pet", 
+    value: "Feed Family Pet", 
+    id: 1 
+  },
+  { 
+    label: "Make Your Bed", 
+    value: "Make Your Bed", 
+    id: 2 
+  },
+  { 
+    label: "Clean Bedroom", 
+    value: "Clean Bedroom", 
+    id: 3 
+  },
+  { label: "Wash Dishes", 
+  value: "Wash Dishes", 
+  id: 4 
+  },
+  { label: "Empty Dishwasher", 
+    value: "Empty Diswasher", 
+    id: 5 
+  },
+  { label: "Fold Laundry", 
+    value: "Fold Laundry", 
+    id: 6 
+  },
+  { label: "Pickup Toys", 
+    value: "Pickup Toys", 
+    id: 7 
+  },
+  {
+    label: "Help Set The Table",
+    value: "Help Set The Table",
+    id: 8
+  },
+  {
+    label: "Pick Up Toys",
+    value: "Pick Up Toys",
+    id: 9
+  },
+  {
+    label: "Rake Leaves",
+    value: "Rake Leaves",
+    id: 10
+  },
+  {
+    label: "Put Away Groceries",
+    value: "Pit Away Groceries",
+    id: 11
+  },
+  {
+    label: "Vacuum Floors",
+    value: "Vacuum Floors",
+    id: 12
+  },
+  {
+    label: "Mow The Lawn",
+    value: "Mow The Lawn",
+    id: 13
+  },
+  {
+    label: "Wash Windows",
+    value: "Wash Windows",
+    id: 14
+  },
+  {
+    label: "Iron Clothes",
+    value: "Iron Clothes",
+    id: 15
+  },
+  {
+    label: "Help Paint",
+    value: "Help Paint",
+    id: 16
+  },
+  {
+    label: "Help Make Meal",
+    value: "Help Make Meal",
+    id: 17
+  }
 ];
 
 export default choresChoices
