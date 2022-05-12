12d8d64d3ce0ce24b270a5e8d4d7bb3b242572b6
millerm30 <millergm30@gmail.com>
2022-05-12 09:49:01 -0230

    remove spinner

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,6 @@ const Header = () => {
       <div className='appLogo flex content-center'>
         <img src={AppLogo} alt='' className='hidden md:block pl-2'/>
         <h1 className='self-center'><span className='text-3xl text-blue-800 font-bold'>Chore</span><span className='text-3xl text-green-800 font-bold'>Bucks</span></h1>
-        <div className='mt-2 ml-4 w-2 h-6 border-2 border-b-gray-900 border-t-red-700 border-l-yellow-600 border-r-green-600 rounded-full animate-spin'></div>
       </div>
       <div className='shoppingBasket flex flex-col items-start'>
         <RiMoneyDollarCircleLine className='text-5xl text-yellow-600' />
