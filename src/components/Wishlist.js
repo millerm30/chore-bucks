0e71c2c0addc4516ddbc1aa50c000513e0664929
millerm30 <millergm30@gmail.com>
2022-05-16 08:50:14 -0230

    set toast for wishlist items

--- src/components/Wishlist.js
+++ src/components/Wishlist.js
@@ -1,13 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import {v4 as uuid} from 'uuid'
 import Wishadd from './Wishadd';
+import { GoTrashcan } from "react-icons/go";
+import toast from "react-hot-toast";
 
 const Wishlist = () => {
   const [wishes, setWishes] = useState(getInitialWishes());
 
   const addWish = (title, points) => {
+    toast.success(`${title} added to wish list!`);
     setWishes([...wishes, {title, points, id: uuid()}])
   }
+
+   const removeWish = (wish) => {
+     toast.error(`${wish.title} removed from wish list!`);
+     setWishes(wishes.filter((i) => i !== wish));
+   };
 
   useEffect(() => {
     const store = JSON.stringify(wishes)
@@ -26,6 +34,11 @@ const Wishlist = () => {
       <div className="wishes grid grid-cols-2 gap-5 py-5 mx-5 md:grid-cols-3 lg:grid-cols-4">
         {wishes.map((wish) => (
           <div key={uuid()} className="wish bg-[#f8f8f8] w-full px-2 py-1 mt-5 flex flex-col justify-start items-center border-2 border-blue-400 rounded-lg">
+            <button
+              onClick={() => removeWish(wish)}
+              className="text-2xl text-red-600 self-end">
+              <GoTrashcan />
+            </button>
             <h2 className='text-xl font-semibold p-1 text-center'>{wish.title}</h2>
             <h3 className='text-lg font-semibold p-1 text-center'>💰{wish.points} Points</h3>
           </div>
