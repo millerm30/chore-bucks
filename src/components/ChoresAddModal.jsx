792d0fb071c4e4ffad29a69f2bbb16c2a26a877e
millerm30 <millergm30@gmail.com>
2022-07-14 22:32:30 -0230

    no click outside modal to close setup

--- src/components/ChoresAddModal.jsx
+++ src/components/ChoresAddModal.jsx
@@ -54,15 +54,11 @@ export default function MyModal() {
     localStorage.setItem("choresList", JSON.stringify(choresList));
   }, [choresList]);
 
-  function closeModal() {
-    setIsOpen(false)
-  }
-
   return (
     <>
       <main className="bg-blue-300">
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+        <Dialog as="div" className="relative z-10" onClose={() => null}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
