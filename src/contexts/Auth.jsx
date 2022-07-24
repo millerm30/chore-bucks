1f5e0b22baacc25bc062ddb44d94b424c7a0aa59
millerm30 <millergm30@gmail.com>
2022-07-24 14:55:55 -0230

    ...

--- src/contexts/Auth.jsx
+++ src/contexts/Auth.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import toast from "react-hot-toast";
 
 const UserContext = React.createContext();
