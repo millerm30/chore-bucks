30b774875de3b3f676d548a8448f3a417fab9b5b
Michael Miller <millergm30@gmail.com>
2022-10-23 15:27:36 -0230

    small change to build path.

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,14 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-const static_dir = path.resolve(path.join(__dirname, 'frontend/build'));
+const static_dir = path.join(__dirname, '../frontend/build');
 
 app.use('/', express.static(static_dir));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+console.log(static_dir);
 
 shared.sayHello();
