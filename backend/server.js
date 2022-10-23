51aaf28767c812c460b6215a930fcfbaae11b72e
Michael Miller <millergm30@gmail.com>
2022-10-23 15:18:31 -0230

    trying to resolve path for build direc.

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require('path');
 app.use(cors());
 app.use(express.json());
 
-const static_dir = path.join(__dirname, '/frontend/build');
+const static_dir = path.resolve(path.join(__dirname, '/frontend/build'));
 
 app.use('/', express.static(static_dir));
 
