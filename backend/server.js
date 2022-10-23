b86ad314aa03d75030570b69b480fe243e29a260
Michael Miller <millergm30@gmail.com>
2022-10-23 15:14:13 -0230

    my oh my

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const cors = require('cors');
 const app = express();
 const shared = require ('shared');
 const path = require('path');
+
+app.use(cors());
+app.use(express.json());
 
 const static_dir = path.join(__dirname, '/frontend/build');
 
