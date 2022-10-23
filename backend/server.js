a70ab45e1e9f6e6d5acb8da8a3105d60e20d3d1c
Michael Miller <millergm30@gmail.com>
2022-10-23 19:15:04 -0230

    last try

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname,'frontend', 'build', 'index.html'));
 });
 
