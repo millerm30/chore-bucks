cd1c33ad97ecae49a8c02cbd29e42fef5fae5a23
Michael Miller <millergm30@gmail.com>
2022-10-23 18:47:39 -0230

    again....

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,13 @@ const static_dir = path.join(__dirname, '../frontend/build');
 
 app.use('/', express.static(static_dir));
 
-isProduction && 
-  app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
