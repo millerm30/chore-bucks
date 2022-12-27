36b06b967eff12f58a84e4ae9e72986f98e16959
millerm30 <millergm30@gmail.com>
2022-12-27 11:27:30 -0330

    database cleanup....

--- backend/database/db.js
+++ backend/database/db.js
@@ -11,99 +11,125 @@ const pool = new Pool({
   port: process.env.PORT,
 });
 
-//Create Tables if not exist
-pool.query(
-  `CREATE TABLE IF NOT EXISTS users (
-    user_id uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
-    user_name VARCHAR(255) NOT NULL,
-    user_email VARCHAR(255) NOT NULL,
-    user_password VARCHAR(255) NOT NULL
-  )`,
-  (err, res) => {
-    if (err) {
-      console.log(err);
-    }
+//Check if Database is connected
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Database Connected");
   }
-);
+});
 
+// Check if Tables are created and if they are return the tables and if not create them with the following code
 pool.query(
-  `CREATE TABLE IF NOT EXISTS predefined_chores (
-    predefined_id uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
-    chore_name VARCHAR(255) NOT NULL,
-    user_id uuid,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
-  )`,
+  `SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`,
   (err, res) => {
     if (err) {
       console.log(err);
-    }
-  }
-);
+    } else {
+      if (res.rows.length === 0) {
+      console.log("No Tables Found");
+      pool.query(
+        `CREATE TABLE IF NOT EXISTS users (
+        user_id uuid PRIMARY KEY DEFAULT 
+        uuid_generate_v4(),
+        user_name VARCHAR(255) NOT NULL,
+        user_email VARCHAR(255) NOT NULL,
+        user_password VARCHAR(255) NOT NULL
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
 
-pool.query(
-  `CREATE TABLE IF NOT EXISTS selected_chores (
-    selected_id uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
-    user_id uuid NOT NULL,
-    chore_value INT NOT NULL,
-    predefined_id uuid NOT NULL,
-    completed BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (predefined_id) REFERENCES predefined_chores (predefined_id)
-  )`,
-  (err, res) => {
-    if (err) {
-      console.log(err);
-    }
-  }
-);
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS predefined_chores (
+        predefined_id uuid PRIMARY KEY DEFAULT 
+        uuid_generate_v4(),
+        chore_name VARCHAR(255) NOT NULL,
+        user_id uuid,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
 
-pool.query(
-  `CREATE TABLE IF NOT EXISTS wishes (
-    wish_id uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
-    wish_name VARCHAR(255) NOT NULL,
-    wish_value INT NOT NULL,
-    user_id uuid,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
-  )`,
-  (err, res) => {
-    if (err) {
-      console.log(err);
-    }
-  }
-);
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS selected_chores (
+        selected_id uuid PRIMARY KEY DEFAULT 
+        uuid_generate_v4(),
+        user_id uuid NOT NULL,
+        chore_value INT NOT NULL,
+        predefined_id uuid NOT NULL,
+        completed BOOLEAN DEFAULT FALSE,
+        FOREIGN KEY (user_id) REFERENCES users (user_id),
+        FOREIGN KEY (predefined_id) REFERENCES predefined_chores (predefined_id)
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
 
-pool.query(
-  `CREATE TABLE IF NOT EXISTS shopping_cart (
-    cart_id uuid PRIMARY KEY DEFAULT 
-    uuid_generate_v4(),
-    item_quantity INT NOT NULL,
-    user_id uuid NOT NULL,
-    wish_id uuid NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users (user_id),
-    FOREIGN KEY (wish_id) REFERENCES wishes (wish_id)
-  )`,
-  (err, res) => {
-    if (err) {
-      console.log(err);
-    }
-  }
-);
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS wishes (
+        wish_id uuid PRIMARY KEY DEFAULT 
+        uuid_generate_v4(),
+        wish_name VARCHAR(255) NOT NULL,
+        wish_value INT NOT NULL,
+        user_id uuid,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
 
-pool.query(
-  `CREATE TABLE IF NOT EXISTS wallet (
-    wallet_id uuid PRIMARY KEY DEFAULT
-    uuid_generate_v4(),
-    user_id uuid NOT NULL,
-    balance INT,
-    FOREIGN KEY (user_id) REFERENCES users (user_id)
-  )`,
-  (err, res) => {
-    if (err) {
-      console.log(err);
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS shopping_cart (
+        cart_id uuid PRIMARY KEY DEFAULT 
+        uuid_generate_v4(),
+        item_quantity INT NOT NULL,
+        user_id uuid NOT NULL,
+        wish_id uuid NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (user_id),
+        FOREIGN KEY (wish_id) REFERENCES wishes (wish_id)
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS wallet (
+        wallet_id uuid PRIMARY KEY DEFAULT
+        uuid_generate_v4(),
+        user_id uuid NOT NULL,
+        balance INT,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+      )`,
+      (err, res) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Tables Created");
+        }
+      }
+    );
+      } else {
+        console.log("Tables Found");
+        console.log(res.rows);
+      }
     }
   }
 );
