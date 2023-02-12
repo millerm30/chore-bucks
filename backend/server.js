74cba995ebeac859673fa5b47feae2e0ef8fc906
millerm30 <millergm30@gmail.com>
2023-02-12 19:37:30 -0330

    test

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,9 @@ const cors = require('cors');
 const app = express();
 const shared = require('shared');
 const { checkAndConnectDB } = require('./database/db');
-const path = require('path');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-});
 
 app.use('/api/auth', require('./routes/jwtAuth'));
 
